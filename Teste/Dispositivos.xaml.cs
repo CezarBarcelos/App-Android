d9e2af33a283920ba9dfdf6430bdaca164f623e9
CEZAR BARCELOS <cezar.opaleiro@gmail.com>
2021-09-08 15:52:11 -0300

    Adicionar arquivos de projeto.

---
+++ Teste/Dispositivos.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Teste
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Dispositivos : ContentPage
+    {
+        public Dispositivos()
+        {
+            InitializeComponent();
+        }
+
+        public void CriaListaDispositivos()
+        {
+            //var adapter = DependencyService.Resolve<IbluetoothAdapter>();
+            //lvBondedDevices.ItemSource = adapter.BondedDevices;
+        }
+    }
+}
